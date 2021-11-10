9cb06bc3916a024afb810ba7a448cf3481ac74e0
bravo <2385097162@qq.com>
2021-11-10 23:18:33 +0800

    fix: 新节点和旧节点处理方式

--- diff.js
+++ diff.js
@@ -7,10 +7,12 @@
 // 当prevChild.mountIndex < lastIndex时对节点进行移动 反之不动 nextChild.mountIndex = nextIndex;
 // 每轮比较 lastIndex = Math.max(prevChild.mountIndex, lastIndex) nextIndex++;
 
-let lastIndex = 0; // 下个要更新节点的位置
-let nextIndex = 0; // 下个要更新节点的下标
-// 对于新的BADC节点
+// 下个要更新节点的位置
+// let lastIndex = 0; 
+// 下个要更新节点的下标
+// let nextIndex = 0;
 
+// 对于新的BADC节点
 // 第一次循环
 // B在老集合中的mountIndex = 1 
 // 1 < 0 不成立故B节点不进行移动; B.mountIndex = 0; lastIndex = Math.max(1, 0); lastIndex = 1; nextIndex = 1;
@@ -25,3 +27,15 @@ let nextIndex = 0; // 下个要更新节点的下标
 // 老集合中C.mountIndex = 2; 2 < 3; 成立; C.mountIndex = 3; lastIndex = Math.max(2, 3); lastIndex = 3; nextIndex = 4; 至此已结束
 
 // 可得到新合集中的mountIndex => B.mountIndex = 0; A.mountIndex = 1; D.mountIndex = 2; C.mountIndex = 3;
+
+
+// * 当有新节点插入和旧节点删除时的处理方式
+// 跟上面情况类似 只不过遇到要插入的时候新建插入 删除时删除 lastIndex和nextIndex都是同样的操作
+// 不过有种情况是最耗时的 ABCD -> DABC 理论上只需要将D移到最前面 但是按照上面的算法显然有多余的操作 
+// 有空可以看看vue的diff算法 好像是采用了首尾双指针的方式
+
+// 看看代码的实现方式
+
+```JS
+
+```
